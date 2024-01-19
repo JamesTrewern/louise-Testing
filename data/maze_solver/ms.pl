f3b65cf42f6dfc86e5f8601ad55e8464163cc3b1
JamesTrewern <trewern.james@gmail.com>
2024-01-19 14:51:11 +0000

    added broken recursive move

--- data/maze_solver/ms.pl
+++ data/maze_solver/ms.pl
@@ -7,6 +7,10 @@
     ,move_down/2
     ,move_left/2
     ,move_right/2
+    ,step_up/2
+    ,step_down/2
+    ,step_left/2
+    ,step_right/2
     ,maze/2
     ,clear/2
 ]).
@@ -33,7 +37,7 @@ background_knowledge(ms/2, [
 
 metarules(ms/2,[chain,identity]).
 
-positive_example(ms/2, ms(s(Maze,[1,0],[]),s(Maze,[1,3],[d,d,d]))):-
+positive_example(ms/2, ms(s(Maze,[1,0],[]),s(Maze,[5,6],[d,r,d]))):-
     maze(0,Maze).
 
 negative_example(ms/2,_):- false.
@@ -46,35 +50,85 @@ clear(Maze,[X,Y]):-
     nth0(X,Row,Cell),
     Cell =:= 1.
 
-move_up(s(Maze,[X,Y],Moves),s(Maze,[X,Y1],[u|Moves])):-
+step_up(s(Maze,[X,Y],M),s(Maze,[X,Y1],M)):-
     Y > 0,
     Y1 is Y - 1,
     clear(Maze,[X,Y1]).
     % writeln(Moves).
-move_down(s(Maze,[X,Y],Moves),s(Maze,[X,Y1],[d|Moves])):-
+step_down(s(Maze,[X,Y],M),s(Maze,[X,Y1],M)):-
     Y < 7,
     Y1 is Y + 1,
     clear(Maze,[X,Y1]).
     % writeln(Moves).
 
-move_left(s(Maze,[X,Y],Moves),s(Maze,[X1,Y],[l|Moves])):-
+step_left(s(Maze,[X,Y],M),s(Maze,[X1,Y],M)):-
     X > 0,
     X1 is X - 1,
     clear(Maze,[X1,Y]).
     % writeln('left').
 
-move_right(s(Maze,[X,Y],Moves),s(Maze,[X1,Y],[r|Moves])):-
+step_right(s(Maze,[X,Y],M),s(Maze,[X1,Y],M)):-
     X < 7,
     X1 is X + 1,
     clear(Maze,[X1,Y]).
     % writeln('right').
+
+move_up(S1,S3):-
+    step_up(S1,S2),
+    ((not(step_up(S2,_));step_left(S2,_);step_right(S2,_))->
+        (
+            s(Maze,P,Moves) = S2,
+            S3 = s(Maze,P,[u|Moves])
+        );
+        (
+            move_up(S2,S3)
+        )
+    ).
+
+move_down(S1,S3):-
+    step_down(S1,S2),
+    ((not(step_down(S2,_));step_left(S2,_);step_right(S2,_))->
+        (
+            s(Maze,P,Moves) = S2,
+            S3 = s(Maze,P,[d|Moves])
+        );
+        (
+            move_down(S2,S3)
+        )
+    ).
+
+move_left(S1,S3):-
+    step_left(S1,S2),
+    ((not(step_left(S2,_));step_up(S2,_);step_down(S2,_))->
+        (
+            s(Maze,P,Moves) = S2,
+            S3 = s(Maze,P,[l|Moves])
+        );
+        (
+            move_left(S2,S3)
+        )
+    ).
+
+move_right(S1,S3):-
+    step_right(S1,S2),
+    ((not(step_right(S2,_));step_up(S2,_);step_down(S2,_))->
+        (
+            s(Maze,P,Moves) = S2,
+            S3 = s(Maze,P,[r|Moves])
+        );
+        (
+            move_right(S2,S3)
+        )
+    ).
 
 
 test:-
     maze(0,Maze),
     S = s(Maze,[1,0],[]),
     move_down(S,S1),
-    move_down(S1,S2),
-    writeln(S2).
+    move_right(S1,S2),
+    move_down(S2,s(_,P,M)),
+    writeln(M),
+    writeln(P).
 
 % debug(learn),debug(prove_steps),debug(fetch),debug(prove_metasubs).
